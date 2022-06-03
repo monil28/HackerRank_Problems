67ee91a7c26ad5153a84a5d143ff83e825e6dd48
monil28 <monushah2812@gmail.com>
2022-06-03 13:10:16 +0530

    Beautiful Triplets Solution

---
+++ beautiful_triplets.py
@@ -0,0 +1,47 @@
+"""
+    https://www.hackerrank.com/challenges/beautiful-triplets/problem
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulTriplets' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def beautifulTriplets(d, arr):
+    # Write your code here
+    c=0
+    for j in range(len(arr)):
+        val = arr[j]+d
+        val1 = arr[j] + 2*d
+        if val in arr and val1 in arr:
+            c += 1
+    return c
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = beautifulTriplets(d, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
